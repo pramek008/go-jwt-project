29de8f3c571b369d4a8449a1bca69c3f2e524fb4
Eka Nova <pramudyaeka.dev@gmail.com>
2024-08-01 14:24:38 +0700

    add utils from the source file

---
+++ utils/jwt_utils.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func GenerateToken(userID uint32) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = userID
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix() // Token expires after 24 hours
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func ValidateToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+}
