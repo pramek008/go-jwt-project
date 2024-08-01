4e7e4991f22fa18ae4796f77d7c88da3e1a5f05d
Eka Nova <pramudyaeka.dev@gmail.com>
2024-08-01 16:59:27 +0700

    add base response in middleware

--- middleware/jwt_middleware.go
+++ middleware/jwt_middleware.go
@@ -13,28 +13,32 @@ func JWTMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			// c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			utils.SendErrorResponse(c, http.StatusUnauthorized, "Authorization header is required")
 			c.Abort()
 			return
 		}
 
 		parts := strings.SplitN(authHeader, " ", 2)
 		if !(len(parts) == 2 && parts[0] == "Bearer") {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header format must be Bearer {token}"})
+			// c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header format must be Bearer {token}"})
+			utils.SendErrorResponse(c, http.StatusUnauthorized, "Authorization header format must be Bearer {token}")
 			c.Abort()
 			return
 		}
 
 		token, err := utils.ValidateToken(parts[1])
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			// c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			utils.SendErrorResponse(c, http.StatusUnauthorized, "Invalid or expired token")
 			c.Abort()
 			return
 		}
 
 		claims, ok := token.Claims.(jwt.MapClaims)
 		if !ok || !token.Valid {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims"})
+			// c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims"})
+			utils.SendErrorResponse(c, http.StatusUnauthorized, "Invalid token claims")
 			c.Abort()
 			return
 		}
