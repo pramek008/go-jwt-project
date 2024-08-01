3e1c9066ff4547d21800414d50786e0d950a9329
Eka Nova <pramudyaeka.dev@gmail.com>
2024-08-01 17:12:46 +0700

    update to uuid

--- models/user.go
+++ models/user.go
@@ -3,11 +3,12 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID        uint           `gorm:"primaryKey"`
+	ID        uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
 	Nickname  string         `gorm:"size:255;not null;unique"`
 	Email     string         `gorm:"size:100;not null;unique"`
 	Password  string         `gorm:"size:100;not null"`
