5b5e7f4efa933af7554cae5473bc2d94ea555e87
Eka Nova <pramudyaeka.dev@gmail.com>
2024-08-01 15:00:03 +0700

    fix named dir

--- routes/routes.go
+++ routes/routes.go
@@ -2,8 +2,8 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	// "github.com/pramek008/go-jwt-project/controllers"
-	// "github.com/pramek008/go-jwt-project/middleware"
+	"github.com/pramek008/go-jwt-project/controllers"
+	"github.com/pramek008/go-jwt-project/middleware"
 )
 
 func SetupRoutes(r *gin.Engine) {
@@ -15,20 +15,20 @@ func SetupRoutes(r *gin.Engine) {
 	})
 
 	// Public routes
-	// public := r.Group("/api")
-	// {
-	// public.POST("/register", controllers.Register)
-	// public.POST("/login", controllers.Login)
-	// }
+	public := r.Group("/api")
+	{
+		public.POST("/register", controllers.Register)
+		public.POST("/login", controllers.Login)
+	}
 
 	// Protected routes
-	// protected := r.Group("/api")
-	// protected.Use(middleware.JWTMiddleware())
-	// {
-	// 	protected.POST("/posts", controllers.CreatePost)
-	// 	protected.GET("/posts/:id", controllers.GetPost)
-	// 	protected.PUT("/posts/:id", controllers.UpdatePost)
-	// 	protected.DELETE("/posts/:id", controllers.DeletePost)
-	// 	protected.GET("/posts", controllers.ListPosts)
-	// }
+	protected := r.Group("/api")
+	protected.Use(middleware.JWTMiddleware())
+	{
+		protected.POST("/posts", controllers.CreatePost)
+		protected.GET("/posts/:id", controllers.GetPost)
+		protected.PUT("/posts/:id", controllers.UpdatePost)
+		protected.DELETE("/posts/:id", controllers.DeletePost)
+		protected.GET("/posts", controllers.ListPosts)
+	}
 }
