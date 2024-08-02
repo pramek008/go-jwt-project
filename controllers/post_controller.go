aad4d73c00b34f8a990647c2d6d7bdebb4d640b7
Eka Nova <pramudyaeka.dev@gmail.com>
2024-08-02 11:33:42 +0700

    update with uuid type

--- controllers/post_controller.go
+++ controllers/post_controller.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/pramek008/go-jwt-project/database"
 	"github.com/pramek008/go-jwt-project/models"
 	"github.com/pramek008/go-jwt-project/utils"
@@ -13,21 +14,18 @@ import (
 func CreatePost(c *gin.Context) {
 	var post models.Post
 	if err := c.ShouldBindJSON(&post); err != nil {
-		// c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		utils.SendErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	userID, _ := c.Get("user_id")
-	post.UserID = userID.(uint)
+	post.UserID = userID.(uuid.UUID)
 
 	if err := database.DB.Db.Create(&post).Error; err != nil {
-		// c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create post"})
 		utils.SendErrorResponse(c, http.StatusInternalServerError, "Failed to create post")
 		return
 	}
 
-	// c.JSON(http.StatusCreated, post)
 	utils.SendResponse(c, http.StatusCreated, true, "Post created successfully", post)
 }
 
@@ -35,13 +33,11 @@ func GetPost(c *gin.Context) {
 	id := c.Param("id")
 
 	var post models.Post
-	if err := database.DB.Db.Preload("User").First(&post, id).Error; err != nil {
-		// c.JSON(http.StatusNotFound, gin.H{"error": "Post not found"})
+	if err := database.DB.Db.Preload("User").First(&post, "id = ?", id).Error; err != nil {
 		utils.SendErrorResponse(c, http.StatusNotFound, "Post not found")
 		return
 	}
 
-	// c.JSON(http.StatusOK, post)
 	utils.SendResponse(c, http.StatusOK, true, "Post fetched successfully", post)
 }
 
@@ -49,27 +45,23 @@ func UpdatePost(c *gin.Context) {
 	id := c.Param("id")
 
 	var post models.Post
-	if err := database.DB.Db.First(&post, id).Error; err != nil {
-		// c.JSON(http.StatusNotFound, gin.H{"error": "Post not found"})
+	if err := database.DB.Db.First(&post, "id = ?", id).Error; err != nil {
 		utils.SendErrorResponse(c, http.StatusNotFound, "Post not found")
 		return
 	}
 
 	userID, _ := c.Get("user_id")
-	if post.UserID != userID.(uint) {
-		// c.JSON(http.StatusForbidden, gin.H{"error": "Not authorized to update this post"})
+	if post.UserID != userID.(uuid.UUID) {
 		utils.SendErrorResponse(c, http.StatusForbidden, "Not authorized to update this post")
 		return
 	}
 
 	if err := c.ShouldBindJSON(&post); err != nil {
-		// c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		utils.SendErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	database.DB.Db.Save(&post)
-	// c.JSON(http.StatusOK, post)
 	utils.SendResponse(c, http.StatusOK, true, "Post updated successfully", post)
 }
 
@@ -77,21 +69,18 @@ func DeletePost(c *gin.Context) {
 	id := c.Param("id")
 
 	var post models.Post
-	if err := database.DB.Db.First(&post, id).Error; err != nil {
-		// c.JSON(http.StatusNotFound, gin.H{"error": "Post not found"})
+	if err := database.DB.Db.First(&post, "id = ?", id).Error; err != nil {
 		utils.SendErrorResponse(c, http.StatusNotFound, "Post not found")
 		return
 	}
 
 	userID, _ := c.Get("user_id")
-	if post.UserID != userID.(uint) {
-		// c.JSON(http.StatusForbidden, gin.H{"error": "Not authorized to delete this post"})
+	if post.UserID != userID.(uuid.UUID) {
 		utils.SendErrorResponse(c, http.StatusForbidden, "Not authorized to delete this post")
 		return
 	}
 
 	database.DB.Db.Delete(&post)
-	// c.JSON(http.StatusOK, gin.H{"message": "Post deleted successfully"})
 	utils.SendResponse[map[string]interface{}](c, http.StatusOK, true, "Post deleted successfully", nil)
 }
 
