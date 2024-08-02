16b36ca3099625b0964925a313c4d3a8a435ce70
Eka Nova <pramudyaeka.dev@gmail.com>
2024-08-02 11:32:42 +0700

    change post with uuid type

--- models/post.go
+++ models/post.go
@@ -3,14 +3,15 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Post struct {
-	ID        uint           `gorm:"primaryKey"`
+	ID        uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
 	Title     string         `gorm:"size:255;not null"`
 	Content   string         `gorm:"type:text;not null"`
-	UserID    uint           `gorm:"not null"`
+	UserID    uuid.UUID      `gorm:"type:uuid;not null"`
 	User      User           `gorm:"foreignKey:UserID"`
 	CreatedAt time.Time      `gorm:"default:CURRENT_TIMESTAMP"`
 	UpdatedAt time.Time      `gorm:"default:CURRENT_TIMESTAMP"`
